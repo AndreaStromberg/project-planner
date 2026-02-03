6e40ea4587fa5d6c3d5d4effbe92f51cafe207b4
Andrea Strömberg <andreastromberg@yahoo.se>
2026-02-03 15:29:08 +0100

    Påbörjat status-funktion

--- src/app.js
+++ src/app.js
@@ -3,6 +3,22 @@ const projectApp = () => {
   const projectForm = document.querySelector("#project-form")
   const projectList = document.querySelector("#project-list")
 
+  //   STATUS OBJECT
+  const PROJECT_STATUS = {
+    archived: {
+      label: "Archived",
+      attr: "archived",
+    },
+    completed: {
+      label: "Completed",
+      attr: "completed",
+    },
+  }
+
+  const setProjectStatus = (id, status) => {
+    console.log("Status for: " + id)
+  }
+
   //   FUNKTION FÖR ATT HÖMTA DATA FRÅN FORMULÄRET
   const getData = () => {
     // Hämta in all data från formuläret, namn, datum, mm, spara som FormData Object
@@ -98,23 +114,18 @@ const projectApp = () => {
                                                     d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                             </svg>
 
-
-
                                             <span>Delete</span>
                                         </button>
                                     </li>
 
                                     <li>
                                         <button
-                                            class="flex items-center gap-2 py-1.5 px-3 text-sm hover:bg-white/5 w-full cursor-pointer text-gray-400 hover:text-gray-100">
+                                            class="archive-button flex items-center gap-2 py-1.5 px-3 text-sm hover:bg-white/5 w-full cursor-pointer text-gray-400 hover:text-gray-100">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                                 stroke="currentColor" class="size-5">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                                             </svg>
-
-
-
 
                                             <span>Archive</span>
                                         </button>
@@ -122,16 +133,12 @@ const projectApp = () => {
 
                                     <li>
                                         <button
-                                            class="flex items-center gap-2 py-1.5 px-3 text-sm hover:bg-white/5 w-full cursor-pointer text-gray-400 hover:text-gray-100">
+                                            class="complete-button flex items-center gap-2 py-1.5 px-3 text-sm hover:bg-white/5 w-full cursor-pointer text-gray-400 hover:text-gray-100">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                                 stroke="currentColor" class="size-5">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                             </svg>
-
-
-
-
 
                                             <span>Complete</span>
                                         </button>
@@ -156,6 +163,12 @@ const projectApp = () => {
     // Delete-knapp
     const deleteButton = projectElement.querySelector(".delete-button")
 
+    // Archive-knapp
+    const archiveButton = projectElement.querySelector(".archive-button")
+
+    // Complete-knapp
+    const completeButton = projectElement.querySelector(".complete-button")
+
     // Skapa eventlyssnare för knappen
     settingsButton.addEventListener("click", () =>
       openProjectSettings(projectId),
@@ -163,6 +176,15 @@ const projectApp = () => {
 
     // Eventlyssnare för deleteknapp
     deleteButton.addEventListener("click", () => deleteProject(projectId))
+
+    // Eventlyssnare för status-knappar
+    archiveButton.addEventListener("click", () =>
+      setProjectStatus(projectId, "archived"),
+    )
+
+    completeButton.addEventListener("click", () =>
+      setProjectStatus(projectId, "completed"),
+    )
   }
 
   // Funktion för att ta bort projekt
